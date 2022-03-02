2c17fc8c8dab6b30f6b3973596084547ed62ecc7
Ibrohim <shohruxmusa@gmail.com>
2022-03-02 12:37:47 +0500

    bazi masalalar
    sariq dev bn

---
+++ hello.py
@@ -0,0 +1,66 @@
+# ismlar=["Hasan", "Shohjahon", "Mirjalol"]
+# print("Salom", ismlar[0], "bugun choyxonaga boramizmi?")
+# print("Salom", ismlar[1], "bugun choyxonaga boramizmi?")
+# print("Salom", ismlar[2], "bugun choyxonaga boramizmi?")
+
+
+
+# ism=["bmw", "tesla", "volvo", "gm"]
+# ism.sort()
+# print(ism)
+
+
+# dav.sort(reverse=True)
+# print(dav)
+
+# sonlar=list(range(1,100,5))
+# sonlar=sonlar.reverse()
+
+# print(sonlar)
+
+# dav=["O'zbekiston", "USA", "Kanada", "Shvetsariya", "Madina"]
+# dav = sorted(dav)
+# print(dav)
+
+
+
+# g_soni=list(range(0, int(input(f"Bugun nechta odam bilan gaplashdingiz:"))))
+# g_odam=[]
+# for odam in g_soni:
+#     g_odam.append(input(f"{odam+1}-insonni kiriting"))
+# print(g_odam)
+
+
+
+# g_soni=int(input(f"Bugun nechta odam bilan gaplashdingiz:"))
+# g_sonlar=[]
+# for go in range(g_soni):
+#     g_sonlar.append(input(f"{go+1}-gaplashgan odamingizni ismi:"))
+    
+# print(g_sonlar)
+
+
+
+# avtos=["malibu","bmw","tesla", "ferrari"]
+
+# for avto in avtos:
+#     if avto=="bmw":
+#         print(avto.upper())
+#     else:
+#             print(avto.title())
+
+
+# ism=input("Ismingiz nima?\n>>>")
+# if ism.lower() != 'ali':
+#     print("Uzur biz Alini kutyabmiz")
+# else:
+#     print("Salom, Ali")
+
+
+
+
+
+
+
+
+
